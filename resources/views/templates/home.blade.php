15bf317e9cd89517c35803ade8eaba9df7b94bc1
michiel-pc\michiel <michiel.abe@gmail.com>
2019-02-28 20:20:24 +0100

    dont show button when no text

--- resources/views/templates/home.blade.php
+++ resources/views/templates/home.blade.php
@@ -75,27 +75,28 @@
                                             ]' data-textAlign="center" data-paddingtop="0" data-paddingright="15" data-paddingbottom="0" data-paddingleft="15">
                                     {{ $slideritem->text }}
                                 </div>
-
-                                <div class="tp-caption rs-btn tp-resizeme" data-x="center" data-hoffset="0" data-y="middle" data-voffset="85" data-width="1170" data-height="none" data-whitespace="normal" data-type="text" data-responsive_offset="on" data-frames='[
-                                                {
-                                                    "delay":1400,
-                                                    "speed":1500,
-                                                    "frame":"0",
-                                                    "from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;",
-                                                    "to":
-                                                        "o:1;",
-                                                        "ease":"Power4.easeInOut"
-                                                },
-                                                {
-                                                    "delay":"wait",
-                                                    "speed":800,
-                                                    "frame":"999",
-                                                    "to":"auto:auto;",
-                                                    "ease":"Power3.easeInOut"
-                                                }
-                                            ]' data-textAlign="center" data-paddingtop="0" data-paddingright="15" data-paddingbottom="0" data-paddingleft="15">
-                                    <a href="{{ $slideritem->button_link }}" class="btn btn-gfort-o" aria-label="knop">{{ $slideritem->button_text }}</a>
-                                </div>
+                                @if(!empty($slideritem->button_text))
+                                    <div class="tp-caption rs-btn tp-resizeme" data-x="center" data-hoffset="0" data-y="middle" data-voffset="85" data-width="1170" data-height="none" data-whitespace="normal" data-type="text" data-responsive_offset="on" data-frames='[
+                                                    {
+                                                        "delay":1400,
+                                                        "speed":1500,
+                                                        "frame":"0",
+                                                        "from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;",
+                                                        "to":
+                                                            "o:1;",
+                                                            "ease":"Power4.easeInOut"
+                                                    },
+                                                    {
+                                                        "delay":"wait",
+                                                        "speed":800,
+                                                        "frame":"999",
+                                                        "to":"auto:auto;",
+                                                        "ease":"Power3.easeInOut"
+                                                    }
+                                                ]' data-textAlign="center" data-paddingtop="0" data-paddingright="15" data-paddingbottom="0" data-paddingleft="15">
+                                        <a href="{{ $slideritem->button_link }}" class="btn btn-gfort-o" aria-label="knop">{{ $slideritem->button_text }}</a>
+                                    </div>
+                                @endif
                             </li>
                             @endforeach
                         </ul>
