100d145f9f3aae051c3a955e2580476deed8bb66
Michiel <michiel.abe@gmail.com>
2019-03-02 13:23:34 +0000

    Texts

--- resources/lang/nl/footer.php
+++ resources/lang/nl/footer.php
@@ -1,10 +1,10 @@
 <?php
 
-return [
-    'contact_title' => 'Contact info',
-    'social_media_title' => 'Volg ons',
-    'block_title' => 'Contacteer ons',
-    'block_text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id ipsum malesuada leo dignissim pulvinar. Nullam lorem erat, rhoncus ut lectus quis, venenatis aliquet mauris. Sed felis diam, pulvinar id velit eu, tincidunt bibendum quam. Integer elementum diam odio, eget tempus magna suscipit eu. ',
-    'block_button' => 'Knop tekst',
-    'block_route' => '1',
-];
+return array (
+  'contact_title' => 'Contact info',
+  'social_media_title' => 'Volg ons',
+  'block_title' => 'vrijblijvend offerte aanvragen',
+  'block_text' => '',
+  'block_button' => 'Vraag offerte aan',
+  'block_route' => '1',
+);
--- resources/lang/nl/home.php
+++ resources/lang/nl/home.php
@@ -1,11 +1,11 @@
 <?php
 
-return [
-    'service_title' => 'SERVICE EN KWALITEIT',
-    'service_text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu aliquet sapien, id rhoncus turpis. Mauris eu euismod libero. Proin egestas molestie quam id pharetra. Integer at mollis risus. ',
-    'realisations' => 'Realisaties',
-    'testimonials_title' => 'Tevreden klanten aan het woord',
-    'services_title' => 'Onze diensten',
-    'contact_title' => 'INTRESSE? VRAGEN?',
-    'contact_text' => 'Laat je gegevens achter.....?',
-];
+return array (
+  'service_title' => 'SERVICE EN KWALITEIT TROEF',
+  'service_text' => 'Wij staan garant voor een professionele, kwalitatieve aanpak en bieden een optimale service voor alles in en rond je tuin. Wij zijn vaklui die beschikken over alle nodige ervaring. Met kennis en kunde maken we van jouw tuin een groene oase waar het zalig is om te vertoeven.',
+  'realisations' => 'Bekijk realisaties',
+  'testimonials_title' => 'Tevreden klanten aan het woord',
+  'services_title' => 'Onze diensten',
+  'contact_title' => 'INTRESSE? VRAGEN?',
+  'contact_text' => 'Laat je gegevens achter.....?',
+);
