974eaf64d32c979c4d03338b413b8f01b6fae81d
michiel-pc\michiel <michiel.abe@gmail.com>
2019-03-02 14:19:40 +0100

    Change favicon to other template

--- resources/views/partials/meta.blade.php
+++ resources/views/partials/meta.blade.php
@@ -26,8 +26,8 @@
     <meta property="og:url" content="{{ Request::url() }}" />
 @endif
 
-<link rel="shortcut icon" href="{{asset('images/favicon.png')}}" type="image/x-icon">
-<link rel="icon" href="{{asset('images/favicon.png')}}" type="image/x-icon">
+<link rel="icon" type="image/png" href="{{asset('images/favicon-32x32.png')}}" sizes="32x32" />
+<link rel="icon" type="image/png" href="{{asset('images/favicon-16x16.png')}}" sizes="16x16" />
 
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
